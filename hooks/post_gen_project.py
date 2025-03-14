1989878f68837885f1a6ce911cab3d9d1e22745e
Wittelsbach-Konig <kiryuschin.vitalij@yandex.ru>
2025-03-14 18:36:26 +0300

    Add git creation script

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,15 @@
+import subprocess
+
+from cookiecutter.utils import read_user_variable
+
+
+def init_git():
+    """Инициализирует Git после генерации проекта"""
+    if read_user_variable("init_git") == "yes":
+        subprocess.run(["git", "init"])
+        subprocess.run(["git", "add", "."])
+        subprocess.run(["git", "commit", "-m", "Initial commit"])
+
+
+if __name__ == "__main__":
+    init_git()
